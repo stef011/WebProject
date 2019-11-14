cd88e7d65466d34f20b4c51f7dc76d0c2505261f
Maxime <max.bs@orange.fr>
2019-11-14 11:04:28 +0100

    register vue

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,27 +2,28 @@
 
 @section ('body')
 
-
 <div class="row mt-3">
     <div class="col-md-4"></div>
     <div class="col-md-4">
         <h2 class="text-center"> Connectez vous à votre espace membre :</h2>
         <div class="component mt-5">
             <form class="text-center text-white mt-5">
-                Identifiant : <input type="text">
+                <input type="text" class="mx-5" placeholder="Identifiant">
             </form>
             <form class="text-center text-white mt-3">
-                Mot de passe : <input type="text">
+                <input type="text" class="mx-5" placeholder="Mot de passe">
+
             </form>
-            <div class="row">
+            <div class="row mt-3">
                 <div class="col-md-4"></div>
-                <div class="buttonform text-center my-2 col-md-4"> Connexion </div>
-                <div class="col-md-4"></div>
+                <div class="buttonform text-center col-md-4"> Connexion </div>
+
             </div>
-            <div class="mt-5 text-center text-white">Tu n'as pas encore de compte ? inscrit toi <a href="#!">ici</a> !</div>
+            <div class="mt-5 mb-3 mx-3 text-center text-white">Tu n'as pas encore de compte ? inscrit toi <a href="#!">ici</a> !</div>
 
         </div>
     </div>
 </div>
 </div>
+
 @endsection
